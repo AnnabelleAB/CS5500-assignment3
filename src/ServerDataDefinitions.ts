6d4794522b896ad10c80455b4f61b8d0c0bd645c
unusG <uniclusgao@gmail.com>
2023-11-27 19:30:25 -0800

    Add render deploy

--- src/ServerDataDefinitions.ts
+++ src/ServerDataDefinitions.ts
@@ -4,4 +4,5 @@ export const PortsGlobal = {
 };
 
 export const LOCAL_SERVER_URL = "http://localhost";
-export const RENDER_SERVER_URL = "https://calc-sheet-backend.onrender.com";
+// export const RENDER_SERVER_URL = "https://calc-sheet-backend.onrender.com";
+export const RENDER_SERVER_URL = "https://cs5500-assignment3-xn1z.onrender.com";
